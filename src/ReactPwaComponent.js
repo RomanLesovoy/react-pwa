a15aca6b1f340633b86ad8fa2ff86a3d197cc215
RomanLesovoy <romadesire97@gmail.com>
2021-02-08 09:40:05 +0200

    Updates

--- src/ReactPwaComponent.js
+++ src/ReactPwaComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../public/logo512.png';
 
 const ReactPwaComponent = () => {
   return (
     <div>
       <h1>React PWA<br />By L. Roman</h1>
-      <img className="logo" src="../public/logo512.png" alt="logo" />
+      <img className="logo" src={logo} alt="logo" />
     </div>
   );
 };
